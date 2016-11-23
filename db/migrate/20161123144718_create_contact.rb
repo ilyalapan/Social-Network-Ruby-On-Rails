cf5f050fb24436f465cd78fd442428918efef11e
Ilya Lapan <lapan.ilya@gmail.com>
2016-11-23 15:42:16 +0000

    Added contacts to nav bar

--- db/migrate/20161123144718_create_contact.rb
+++ db/migrate/20161123144718_create_contact.rb
@@ -1,8 +1,10 @@
 class CreateContact < ActiveRecord::Migration
   def change
+        drop_table :contacts
     create_table :contacts do |t|
       t.string :name
       t.string :email
+      t.text :comments
       
       t.timestamps
     end
